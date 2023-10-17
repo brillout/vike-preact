6140ff099b5b75cc40f61e81cc55b59af855dc34
Romuald Brillout <git@brillout.com>
2023-10-17 15:53:10 +0200

    init

---
+++ .test-preview.test.ts
@@ -0,0 +1,3 @@
+import { isWindows } from '@brillout/test-e2e'
+import { testRun } from '../../boilerplates/.testRun'
+testRun('npm run preview', { uiFramewok: 'preact' })
---
+++ renderer/+config.h.js
@@ -0,0 +1,7 @@
+// https://vike.dev/config
+export default {
+  // https://vike.dev/passToClient
+  passToClient: ['pageProps', 'documentProps'],
+  // https://vike.dev/clientRouting
+  clientRouting: true
+}
